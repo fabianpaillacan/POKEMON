0712586867b016ad785a986085b813bffde2907e
fabianpaillacan <fabianpaillacanh@gmail.com>
2025-08-13 19:29:19 -0400

    Implementar enrutamiento en la aplicación utilizando react-router-dom, configurando rutas para la página de inicio, PokeGrid y Pokedex.

--- pokeapp/src/main.jsx
+++ pokeapp/src/main.jsx
@@ -1,10 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css'
-import App from './App.jsx'
+//import App from './App.jsx'
+import LandingPage from './pages/LandingPage.jsx'
+import PokeGrid from './pages/PokeGridPage.jsx'
+import PokeDex from './pages/PokedexPage.jsx'
 
-createRoot(document.getElementById('root')).render(
+const routes = [
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "/pokegrid",
+    element: <PokeGrid/>,
+  },
+  {
+    path: "/pokedex/:id",
+    element: <PokeDex/>,
+  },
+
+];
+
+const router = createBrowserRouter(routes);
+
+const root = createRoot(document.getElementById('root'));
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
